79e9d2cf5305dad4fa1276bfca98e414dba14fc2
Nix <git@starnix.dev>
2025-07-04 14:26:02 -0400

    bd0b24f

--- machines/pi1/rtlsdr.nix
+++ machines/pi1/rtlsdr.nix
@@ -60,7 +60,7 @@ in {
   };
 
   virtualisation.oci-containers.containers."audiobrowser" = {
-    image = "ghcr.io/nixfey/audiobrowser:sha-8c80465";
+    image = "ghcr.io/nixfey/audiobrowser:sha-bd0b24f";
     autoStart = true;
     volumes = [
       "/conf/rtlsdr-airband/mp3:/files"
