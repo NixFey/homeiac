062c2e4cdb11dcedaf2c7d5d314c2f969e542230
Nix <git@starnix.dev>
2025-07-06 10:03:28 -0400

    homeassistant 2025.7.1

--- machines/pi1/home-assistant.nix
+++ machines/pi1/home-assistant.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   virtualisation.oci-containers.containers.home-assistant = {
-    image = "ghcr.io/home-assistant/home-assistant:2024.12.5";
+    image = "ghcr.io/home-assistant/home-assistant:2025.7.1";
     autoStart = true;
     volumes = [ "/conf/homeassistant/config:/config" ];
     extraOptions = [
